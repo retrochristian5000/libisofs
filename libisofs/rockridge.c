ad5996e3df74570629fa2e43a8be9d28c6886714
Thomas Schmitt <scdbackup@gmx.net>
2025-10-03 13:50:55 +0200

    Introduced proper assessment and curbing of long form timestamps if iso_write_opts_set_replace_timestamps() is not set to default value 0

--- libisofs/rockridge.c
+++ libisofs/rockridge.c
@@ -238,24 +238,18 @@ int rrip_add_TF(Ecma119Image *t, Ecma119Node *n, struct susp_info *susp,
 
     iso = n->node;
     if (n->rrip_tf_long) {
-        node_time= iso->mtime;
+        node_time=  t->replace_timestamps ? t->timestamp : iso->mtime;
         if (t->opts->rrip_tf_y1900 && node_time < ISO_RR_SHORT_FORM_TIME_START)
             node_time= ISO_RR_SHORT_FORM_TIME_START;
-        iso_datetime_17(&TF[5],
-                        t->replace_timestamps ? t->timestamp : node_time,
-                        t->opts->always_gmt);
-        node_time= iso->atime;
+        iso_datetime_17(&TF[5], node_time, t->opts->always_gmt);
+        node_time= t->replace_timestamps ? t->timestamp : iso->atime;
         if (t->opts->rrip_tf_y1900 && node_time < ISO_RR_SHORT_FORM_TIME_START)
             node_time= ISO_RR_SHORT_FORM_TIME_START;
-        iso_datetime_17(&TF[5 + 17],
-                        t->replace_timestamps ? t->timestamp : node_time,
-                        t->opts->always_gmt);
-        node_time= iso->ctime;
+        iso_datetime_17(&TF[5 + 17], node_time, t->opts->always_gmt);
+        node_time= t->replace_timestamps ? t->timestamp : iso->ctime;
         if (t->opts->rrip_tf_y1900 && node_time < ISO_RR_SHORT_FORM_TIME_START)
             node_time= ISO_RR_SHORT_FORM_TIME_START;
-        iso_datetime_17(&TF[5 + 2 * 17],
-                        t->replace_timestamps ? t->timestamp : node_time,
-                        t->opts->always_gmt);
+        iso_datetime_17(&TF[5 + 2 * 17], node_time, t->opts->always_gmt);
     } else {
         iso_datetime_7(&TF[5],
                        t->replace_timestamps ? t->timestamp : iso->mtime,
@@ -1537,6 +1531,7 @@ static
 void iso_decide_rrip_tf_form(Ecma119Image *t, Ecma119Node *n)
 {
     IsoNode *iso_node;
+    time_t node_time;
 
     /* The decision for long form cannot be revoked */
     if (n->rrip_tf_long)
@@ -1548,19 +1543,21 @@ void iso_decide_rrip_tf_form(Ecma119Image *t, Ecma119Node *n)
         iso_node= n->node;
         if (sizeof(iso_node->atime) > 4) {
             /* Check [acm]time for year 1900 and (nearly) year 2156 */
-            if (iso_node->atime > ISO_RR_SHORT_FORM_TIME_LIMIT ||
-                (iso_node->atime < ISO_RR_SHORT_FORM_TIME_START &&
-                 !t->opts->rrip_tf_y1900)) {
+            node_time=  t->replace_timestamps ? t->timestamp : iso_node->atime;
+            if (node_time > ISO_RR_SHORT_FORM_TIME_LIMIT ||
+                (node_time < ISO_RR_SHORT_FORM_TIME_START &&
+                 !t->opts->rrip_tf_y1900))
                 n->rrip_tf_long = 1;
-            } else if (iso_node->ctime > ISO_RR_SHORT_FORM_TIME_LIMIT ||
-                       (iso_node->ctime < ISO_RR_SHORT_FORM_TIME_START &&
-                        !t->opts->rrip_tf_y1900)) {
+            node_time=  t->replace_timestamps ? t->timestamp : iso_node->ctime;
+            if (node_time > ISO_RR_SHORT_FORM_TIME_LIMIT ||
+                (node_time < ISO_RR_SHORT_FORM_TIME_START &&
+                 !t->opts->rrip_tf_y1900))
                 n->rrip_tf_long = 1;
-            } else if (iso_node->mtime > ISO_RR_SHORT_FORM_TIME_LIMIT ||
-                       (iso_node->mtime < ISO_RR_SHORT_FORM_TIME_START &&
-                        !t->opts->rrip_tf_y1900)) {
+            node_time=  t->replace_timestamps ? t->timestamp : iso_node->mtime;
+            if (node_time > ISO_RR_SHORT_FORM_TIME_LIMIT ||
+                (node_time < ISO_RR_SHORT_FORM_TIME_START &&
+                 !t->opts->rrip_tf_y1900))
                 n->rrip_tf_long = 1;
-            }
         }
     }
 }
